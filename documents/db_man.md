e54100a84694fac4b8933c96d679ae119fde579a
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-11-26 12:07:21 +0200

    added test data in schema.sql

---
+++ documents/db_man.md
@@ -0,0 +1,5 @@
+## Database usage and structure
+
+At the time of writing (26.11.2021 12:02) there is a single table called 'articles'. 
+
+For testing purposes there is a DROP TABLE-command before creating the table, so that the isn't 
--- schema.sql
+++ schema.sql
@@ -1,6 +1,30 @@
+DROP TABLE articles;
+
 CREATE TABLE articles (
   id SERIAL PRIMARY KEY, 
   title TEXT, 
   author TEXT, 
   url TEXT
 );
+
+
+
+
+
+
+
+-- Some inserts into db so that there is sample data
+INSERT INTO articles (
+  title, 
+  author, 
+  url
+  ) VALUES (
+    'How to brew a cup of coffee', 
+    'James B. Rew', 
+    'http://www.google.com'
+  )
+
+
+
+
+
