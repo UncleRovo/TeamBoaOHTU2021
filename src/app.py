f171ec41cb80e962b1de1d7ffd3b5efcd2931be4
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-11-26 12:27:05 +0200

    updated db_man and added db initialization in app.py. Database queries now work (example query done in articles.get_all() else branch.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Defines address for db and creates db-object that can execute sql-commands
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///juhanaka"
+db = SQLAlchemy(app)
+
 
 import routes
--- src/articles.py
+++ src/articles.py
@@ -1,15 +1,18 @@
-#from db import db
+from app import db
 from collections import namedtuple
 
 def get_all():
     if True:
+        
+
         Article = namedtuple("Article", "id title author url")
         google = Article(1, "google", "sä ja mä", "https://www.google.com")
         yle = Article(2, "yle", "yleiset", "https://www.yle.fi")
         
         return [google, yle]
     else:
-        query = "SELECT * FROM  article"
-        result = db.session.execute(query)
+        # Esimerkki toimivasta kyselystä (Juhana)
+        sql = 'SELECT * FROM articles'
+        result = db.session.execute(sql)
         articles = result.fetchall()
-    return articles
+        return articles
