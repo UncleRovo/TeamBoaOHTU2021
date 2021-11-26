b4e5603b4f70747a0778b6b456a1ea2e51064f58
Petro Lehtonen <petro.lehtonen@gmail.com>
2021-11-26 13:15:07 +0200

    cleaned up articles.py

--- src/articles.py
+++ src/articles.py
@@ -2,20 +2,9 @@ from app import db
 from collections import namedtuple
 
 def get_all():
-    # Laitoin funktion palauttamaan normaalin kyselyn tuloksen väliaikaisratkaisun sijaan :) -t. Juhana
     sql = 'SELECT * FROM article'
     result = db.session.execute(sql)
     articles = result.fetchall()
     return articles
-    
-    # if True:
-        
-
-    #     Article = namedtuple("Article", "id title author url")
-    #     google = Article(1, "google", "sä ja mä", "https://www.google.com")
-    #     yle = Article(2, "yle", "yleiset", "https://www.yle.fi")
-        
-    #     return [google, yle]
-    # else:
 
     
