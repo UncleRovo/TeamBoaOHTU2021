324b4b023eed66cd6ef49230015499d377123f76
Amanda <amanda.hamynen@gmail.com>
2021-11-27 13:13:42 +0200

    Fixed article -> articles

--- src/articles.py
+++ src/articles.py
@@ -2,7 +2,7 @@ from app import db
 from collections import namedtuple
 
 def get_all():
-    sql = 'SELECT * FROM article'
+    sql = 'SELECT * FROM articles'
     result = db.session.execute(sql)
     articles = result.fetchall()
     return articles
