666ceb4db1e21c247e400f08a79ebf00a31bac60
karhelmi <helmi.karesti@helsinki.fi>
2021-11-27 16:49:03 +0200

    Sprint1: User story Add new article code and related test added - version#1

--- src/articles.py
+++ src/articles.py
@@ -1,10 +1,14 @@
 from app import db
 from collections import namedtuple
 
+def add_new_article(article_title, article_author, article_url):
+    sql = 'INSERT INTO articles (title, author, url) VALUES (:title, :author, :url)'
+    db.session.execute(sql, {"title":article_title, "author":article_author, "url":article_url})
+    db.session.commit()
+    return True
+
 def get_all():
     sql = 'SELECT * FROM articles'
     result = db.session.execute(sql)
     articles = result.fetchall()
     return articles
-
-    
