1a2fe9e797ad2a3e669a72f11412156fbed50570
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-11-28 19:50:08 +0200

    Improved articles

--- src/articles.py
+++ src/articles.py
@@ -1,14 +1,19 @@
 from app import db
 from collections import namedtuple
 
-def add_new_article(article_title, article_author, article_url):
-    sql = 'INSERT INTO articles (title, author, url) VALUES (:title, :author, :url)'
-    db.session.execute(sql, {"title":article_title, "author":article_author, "url":article_url})
-    db.session.commit()
-    return True
+class Articles:
 
-def get_all():
-    sql = 'SELECT * FROM articles'
-    result = db.session.execute(sql)
-    articles = result.fetchall()
-    return articles
+    def __init__(self):
+        self.articles = None
+        
+    def add_new_article(article_title, article_author, article_url):
+        sql = 'INSERT INTO articles (title, author, url) VALUES (:title, :author, :url)'
+        db.session.execute(sql, {"title":article_title, "author":article_author, "url":article_url})
+        db.session.commit()
+        return True
+
+    def get_all():
+        sql = 'SELECT * FROM articles'
+        result = db.session.execute(sql)
+        articles = result.fetchall()
+        return articles
