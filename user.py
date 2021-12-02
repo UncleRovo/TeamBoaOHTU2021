77091df60974a8469dcc31d0a04a54dd1c3d87be
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-12-02 18:57:38 +0200

    some formatting

--- user.py
+++ user.py
@@ -1,28 +1,28 @@
-from app import db
-from werkzeug.security import check_password_hash, generate_password_hash
 from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+from app import db
 
 def login(username, password):
-		sql = 'SELECT id, username, password FROM users WHERE username=:username'
-		result = db.session.execute(sql, {'username':username})
-		user = result.fetchone()
-		if user != None:
-			if check_password_hash(user[2], password):
-				session['username'] = user[1]
-				session['user_id'] = user[0]
-				return True
-		return False
+	sql = "SELECT id, username, password FROM users WHERE username=:username"
+	result = db.session.execute(sql, {"username":username})
+	user = result.fetchone()
+	if user is not None:
+		if check_password_hash(user[2], password):
+			session["username"] = user[1]
+			session["user_id"] = user[0]
+			return True
+	return False
 
 def register(username, password):
-		try:
-			hash_value = generate_password_hash(password)
-			sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-			db.session.execute(sql, {"username":username, "password":hash_value})
-			db.session.commit()
-			return True
-		except:
-			return False
+	try:
+		hash_value = generate_password_hash(password)
+		sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+		db.session.execute(sql, {"username":username, "password":hash_value})
+		db.session.commit()
+		return True
+	except:
+		return False
 
 def logout():
-		session.clear()
-		return True
+	session.clear()
+	return True
