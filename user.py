4f14c0ad10d9271f22959377a7ea593c59380009
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-12-02 19:06:38 +0200

    added exception for scoped-classes if pylint (pylint as static code checker and db connections are not)

--- user.py
+++ user.py
@@ -3,25 +3,25 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from app import db
 
 def login(username, password):
-	sql = "SELECT id, username, password FROM users WHERE username=:username"
-	result = db.session.execute(sql, {"username":username})
-	user = result.fetchone()
-	if user is not None:
-		if check_password_hash(user[2], password):
-			session["username"] = user[1]
-			session["user_id"] = user[0]
-			return True
-	return False
+    sql = "SELECT id, username, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user is not None:
+        if check_password_hash(user[2], password):
+            session["username"] = user[1]
+            session["user_id"] = user[0]
+            return True
+    return False
 
 def register(username, password):
-	try:
-		hash_value = generate_password_hash(password)
-		sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-		db.session.execute(sql, {"username":username, "password":hash_value})
-		db.session.commit()
-		return True
-	except:
-		return False
+    try:
+        hash_value = generate_password_hash(password)
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+        return True
+    except: # pylint: disable=bare-except
+        return False
 
 def logout():
 	session.clear()
