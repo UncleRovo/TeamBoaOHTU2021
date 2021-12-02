4fe5c83673054c20658d14209ae15f3a02b7ad8e
Amanda <amanda.hamynen@gmail.com>
2021-12-02 14:02:05 +0200

    Added books.py

---
+++ books.py
@@ -0,0 +1,13 @@
+from app import db
+
+def add_new_book(title, author, isbn):
+    sql = 'INSERT INTO article (title, author, isbn) VALUES (:title, :author, :isbn)'
+    db.session.execute(sql, {"title":title, "author":author, "isbn":isbn})
+    db.session.commit()
+    return True
+
+def get_all():
+    sql = 'SELECT * FROM book WHERE visible=1'
+    result = db.session.execute(sql)
+    books = result.fetchall()
+    return books
