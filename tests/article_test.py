4cc6eb14f3421f87ad0ca88b18c25873d750a5d9
Petro Lehtonen <petro.lehtonen@gmail.com>
2021-12-03 09:49:49 +0200

    Unit tests for fetching all blogs and articles from database

--- tests/article_test.py
+++ tests/article_test.py
@@ -8,3 +8,10 @@ class TestArticle(unittest.TestCase):
     def test_article_added_to_database(self):
         success = articles.add_new_article("ABC", "Virtanen", "", "www.abc.fi")
         self.assertEqual(success, True)
+
+    def test_all_articles_fetched_from_database(self):
+        amount = len(articles.get_all())
+        self.assertGreaterEqual(amount, 2)
+
+        articles.add_new_article("Testing stuff", "T. Esting", "10.9876/54321", "")
+        self.assertEqual(len(articles.get_all()), amount + 1)
--- tests/blog_test.py
+++ tests/blog_test.py
@@ -8,3 +8,10 @@ class TestArticle(unittest.TestCase):
     def test_blog_added_to_database(self):
         success = blogs.add_new_blog("ABC", "Virtanen", "www.abc.fi")
         self.assertEqual(success, True)
+
+    def test_all_blogs_fetched_from_database(self):
+        amount = len(blogs.get_all())
+        self.assertGreaterEqual(amount, 1)
+
+        blogs.add_new_blog("Best chocolate cake recipe", "mummi", "https://www.google.com")
+        self.assertEqual(len(blogs.get_all()), amount + 1)
